c2b6f3a0a51969e83176f85f72b0f29622e66994
Michael Clifford <cliffom@gmail.com>
2024-02-06 11:35:34 -0800

    Add viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Viewport, Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -7,6 +7,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "cloudvane.cliffom.net"
 };
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1.0,
+  maximumScale: 1.0,
+  userScalable: false
+}
 
 export default function RootLayout({
   children,
